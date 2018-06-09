37258d8037ee9f0bd151be6666b6b6918dae0521
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-06-09 12:43:17 +0100

    Adds firebase.

---
+++ config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: 'viatorem-20e52.firebaseapp.com',
+  databaseURL: 'https://viatorem-20e52.firebaseio.com',
+  projectId: 'viatorem-20e52',
+  storageBucket: '',
+  messagingSenderId: '238385031324'
+}
--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,7 @@
 import App from './src/app'
+import * as firebase from 'firebase'
+import { firebaseConfig } from './config'
+
+firebase.initializeApp(firebaseConfig)
 
 const app = new App()
