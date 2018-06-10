14dc384a0cbb403f8d6ba6f5501244e3efb82eae
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-06-10 15:04:05 +0100

    Adds form.

--- src/containers/Auth/Login/LoginContainer.js
+++ src/containers/Auth/Login/LoginContainer.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 
 class LoginContainer extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
   render () {
     return (
       <View>
-        <Text>LoginContainer</Text>
+        <Text>Log In</Text>
+        <TextInput 
+          value={this.state.username}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({ username: text })}
+        />
       </View>
     )
   }
