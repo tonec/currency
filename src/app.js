a944f6b733697aac5353a443cf470134ad6c34a0
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-06-10 14:02:36 +0100

    Refactors screens.js for multiple configurations.

--- src/app.js
+++ src/app.js
@@ -1,45 +1,43 @@
-import { AppRegistry } from 'react-native'
+import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { Navigation } from 'react-native-navigation'
 import configureStore from './redux/configureStore'
-import { registerScreens } from './screens'
-import { Navigation as MainNav } from './components'
+import { registerAuthScreens, registerMainScreens } from './screens'
 import { iconsLoaded } from './utils/icons'
-import LoginContainer from './containers/Auth/Login/LoginContainer'
+import { Navigation as MainNav } from './components'
 
 const store = configureStore()
 
-export const init = () => {
+export const init = async () => {
   
-  iconsLoaded.then(() => {
-    startAuth()
-  })
+  await iconsLoaded
 
-  // const token = await AsyncStorage.getItem('authToken')
+  const token = await AsyncStorage.getItem('authToken')
   // const token = await AsyncStorage.removeItem(authToken)
 
-  // if (!token) {
-  //   // startLogin();
-  //   startMainApp();
-  // } else {
-  //   startMainApp();
-  // }
+  if (!token) {
+    startAuth()
+  } else {
+    startMain()
+  }
 }
 
 const startAuth = () => {
-  Navigation.registerComponent('viatorem.Login', () => LoginContainer);
-  Navigation.startSingleScreenApp({
-    screen: {
-      screen: 'viatorem.Login',
-      title: 'Log In'
-    }
+  persistStore(store, null, () => {
+    registerAuthScreens(store, Provider)
+    Navigation.startSingleScreenApp({
+      screen: {
+        screen: 'viatorem.Login',
+        title: 'Log In'
+      }
+    })
   })
 }
 
 const startMain = () => {
   persistStore(store, null, () => {
-    registerScreens(store, Provider)
+    registerMainScreens(store, Provider)
     MainNav()
   })
 }
--- src/screens.js
+++ src/screens.js
@@ -1,9 +1,15 @@
 import { Navigation } from 'react-native-navigation'
 
+import LoginContainer from './containers/Auth/Login/LoginContainer'
+
 import ConverterListContainer from './containers/Converter/ConverterListContainer'
 import CurrencyListContainer from './containers/Currencies/CurrencyListContainer'
 
-export function registerScreens (store, Provider) {
+export const registerAuthScreens = (store, Provider) => {
+  Navigation.registerComponent('viatorem.Login', () => LoginContainer, store, Provider)
+}
+
+export const registerMainScreens = (store, Provider) => {
   Navigation.registerComponent('viatorem.Conversion', () => ConverterListContainer, store, Provider)
   Navigation.registerComponent('viatorem.Currencies', () => CurrencyListContainer, store, Provider)
 }
