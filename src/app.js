a4ef95cc6b03b52242abc765b24d78e44311f557
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-06-11 12:22:44 +0100

    adds firebase.

--- src/app.js
+++ src/app.js
@@ -2,46 +2,54 @@ import { AsyncStorage } from 'react-native'
 import { Provider } from 'react-redux'
 import { persistStore } from 'redux-persist'
 import { Navigation } from 'react-native-navigation'
+import * as firebaseApi from 'firebase'
+import { firebaseConfig } from '../config'
 import configureStore from './redux/configureStore'
 import { registerAuthScreens, registerMainScreens } from './screens'
 import { iconsLoaded } from './utils/icons'
 import { Navigation as MainNav } from './components'
 
-const store = configureStore()
+let firebase
+
+if (!firebaseApi.apps.length) {
+  firebase = firebaseApi.initializeApp(firebaseConfig)
+}
+
+const store = configureStore(firebase)
 
 export const init = async () => {
-  
   await iconsLoaded
 
-  const token = await AsyncStorage.getItem('authToken')
-  // const token = await AsyncStorage.removeItem(authToken)
+  persistStore(store, null, () => {
 
-  if (!token) {
-    startAuth()
-  } else {
-    startMain()
-  }
+    const token = false
+    // const token = await AsyncStorage.removeItem(authToken)
+    
+    console.log('auth', store.getState().auth.user)
+
+    if (!token) {
+      startAuth()
+    } else {
+      startMain()
+    }
+  })
 }
 
 const startAuth = () => {
-  persistStore(store, null, () => {
-    registerAuthScreens(store, Provider)
-    Navigation.startSingleScreenApp({
-      screen: {
-        screen: 'viatorem.Login',
-        title: '',
-        navigatorStyle: {
-          navBarNoBorder: true
-        }
+  registerAuthScreens(store, Provider)
+  Navigation.startSingleScreenApp({
+    screen: {
+      screen: 'viatorem.Login',
+      title: '',
+      navigatorStyle: {
+        navBarNoBorder: true
       }
-    })
+    }
   })
 }
 
 const startMain = () => {
-  persistStore(store, null, () => {
-    registerMainScreens(store, Provider)
-    MainNav()
-  })
+  registerMainScreens(store, Provider)
+  MainNav()
 }
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,36 +2,29 @@
 import { createStore, applyMiddleware } from 'redux'
 import { persistCombineReducers } from 'redux-persist'
 import storage from 'redux-persist/es/storage'
-import * as firebaseApi from 'firebase'
-import { firebaseConfig } from '../../config'
 import apiClient from '../utils/apiClient'
 import clientMiddleware from './middleware/clientMiddleware'
 import rootReducer from './rootReducer'
 
-let firebase
+export default function configureStore (firebase, initialState = {}) {
 
-if (!firebaseApi.apps.length) {
-  firebase = firebaseApi.initializeApp(firebaseConfig)
-}
+  let middleware = [clientMiddleware({ client: apiClient(), firebase })]
 
-let middleware = [clientMiddleware({ client: apiClient(), firebase })]
+  const config = {
+    key: 'root',
+    storage
+  }
 
-const config = {
-  key: 'root',
-  storage
-}
+  const reducer = persistCombineReducers(config, rootReducer)
 
-const reducer = persistCombineReducers(config, rootReducer)
+  if (process.env.NODE_ENV === 'development') {
+    const logger = require('redux-logger').createLogger()
+    const reduxImmutableStateInvariant = require('redux-immutable-state-invariant').default()
+    middleware = [ ...middleware, reduxImmutableStateInvariant, logger.__esModule ? logger.default : logger ]
+  } else {
+    middleware = [ ...middleware ]
+  }
 
-if (process.env.NODE_ENV === 'development') {
-  const logger = require('redux-logger').createLogger()
-  const reduxImmutableStateInvariant = require('redux-immutable-state-invariant').default()
-  middleware = [ ...middleware, reduxImmutableStateInvariant, logger.__esModule ? logger.default : logger ]
-} else {
-  middleware = [ ...middleware ]
-}
-
-export default function configureStore (initialState) {
   return createStore(
     reducer,
     initialState,
